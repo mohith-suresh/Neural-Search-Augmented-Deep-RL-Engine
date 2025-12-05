30a1ee47d1066350ac654ef75308950ba6eabefc
Krish Modi <krish.modi.2000@gmail.com>
2025-12-04 20:28:12 -0800

    update the config for more utilisation

--- FINAL/chess_ai/game_engine/main.py
+++ FINAL/chess_ai/game_engine/main.py
@@ -25,10 +25,10 @@ ITERATIONS = 1000             # Just 1 loop to verify the high-quality settings
 
 # Auto-detect CPUs: Leave 2 cores free for OS & GPU Server
 # On a 32-core cloud VM, this gives 30 workers.
-NUM_WORKERS = max(1, mp.cpu_count() - 2)
+NUM_WORKERS = 2 * (max(1, mp.cpu_count() - 2))
 
 # Generation
-GAMES_PER_WORKER = 10       # Lower batch size per worker for higher parallelism
+GAMES_PER_WORKER = 5       # Lower batch size per worker for higher parallelism
                            # Total Games/Iter = 30 workers * 10 = 150 games
 
 # --- QUALITY ---
