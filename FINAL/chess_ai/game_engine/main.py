54673fb4da0e5e13b1ad3bc663abd978c17dd8e6
Krish Modi <krish.modi.2000@gmail.com>
2025-12-12 15:58:21 -0800

    hyperparameter tuning

--- FINAL/chess_ai/game_engine/main.py
+++ FINAL/chess_ai/game_engine/main.py
@@ -244,8 +244,8 @@ else:
 # Training
 TRAIN_EPOCHS = 3 
 TRAIN_WINDOW = 40           
-TRAIN_BATCH_SIZE = 3072      
-TRAIN_LR = 0.0001          
+TRAIN_BATCH_SIZE = 2048
+TRAIN_LR = 0.000075       
 
 # --- DRY WORKER WRAPPERS ---
 
