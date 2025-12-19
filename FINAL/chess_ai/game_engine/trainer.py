8af35fd8fa7722f7c08087b385b8ec557d669843
Krish Modi <krish.modi.2000@gmail.com>
2025-12-19 08:53:10 +0800

    hyperparameter tuning

--- FINAL/chess_ai/game_engine/trainer.py
+++ FINAL/chess_ai/game_engine/trainer.py
@@ -162,7 +162,7 @@ def train_model(data_path="data/self_play",
         batch_size=batch_size,
         shuffle=True,
         num_workers=15,
-        prefetch_factor=2,          
+        prefetch_factor=4,          
         pin_memory=True,            
         persistent_workers=True  
     )
